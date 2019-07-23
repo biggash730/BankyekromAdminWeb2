41cc704c5e14e405dacbb8f204a99ea6ea31e0fc
Francis Miah <francis@axoninfosystems.com>
2019-07-23 12:58:33 +0000

    working on the dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -24,59 +24,31 @@ interface SustainabilityStatusQuery {
 })
 export class DashboardComponent implements OnInit, AfterViewInit {
 
-  subscriberTypes$: Observable<Lookup[]>
-  loadingSubscriberTypes: boolean
-  areas$: Observable<Lookup[]>
-  loadingAreas: boolean
-  pillars$: Observable<Lookup[]>
-  loadingPillars: boolean
-  topics$: Observable<Lookup[]>
-  loadingTopics: boolean
-  programs$: Observable<Lookup[]>
-  loadingPrograms: boolean
-  districts$: Observable<Lookup[]>
-  loadingDistricts: boolean
-  subscriberTypeId: number
-  subscriberTypeSummary$: Observable<any>
-  subscriberSummary$: Observable<any>
-  susStatus$: Observable<any>
-  @BlockUI('type') blockSubscriberType: NgBlockUI
-  @BlockUI('subs') blockSubscriberSummary: NgBlockUI
-  @BlockUI('commodity') blockCommodity: NgBlockUI
-  @BlockUI('land') blockLand: NgBlockUI
-  @BlockUI('campaign') blockCampaign: NgBlockUI
+  generalStats$: Observable<any[]>
+  loadingGeneralStats: boolean
+  yearlyStats$: Observable<Lookup[]>
+  loadingYearlyStats: boolean
+  @BlockUI('generalstats') blockGeneralStats: NgBlockUI
+  @BlockUI('yearlystats') blockYearlyStats: NgBlockUI
   doughnut = {}
   pie = {}
   @ViewChild('doughnutcanvas') doughnutcanvas: ElementRef
   @ViewChild('piecanvas') piecanvas: ElementRef
-  startDate: string
-  endDate: string
-  filter = <SustainabilityStatusQuery>{}
+  year: number
+  years: number[]
 
-  constructor(private settingsService: SettingsService,
-    private dashboardService: DashboardService) { }
+  constructor(private dashboardService: DashboardService) { }
 
   ngOnInit() {
-    // this.initDates()
-    // this.loadSubscriberTypes()
-    // this.getSubscriberSummary()
-    // this.loadAreas()
-    // this.loadPillars()
-    // this.loadPrograms()
-    // this.loadDistrict()
-    // this.getSustainabilityStatistics(this.filter)
+    this.year = new Date().getFullYear();
+    this.years = [this.year, this.year - 1, this.year - 2, this.year - 3, this.year -  4, this.year - 5]
+    this.getGeneralStats();
+    this.getYearlyStats();
   }
 
   ngAfterViewInit() {
     // this.landAreaDoughnut()
     // this.subscriberCommodityPie()
-  }
-
-  subscriberTypeOnChange() {
-    this.blockSubscriberType.start('Loading...')
-    this.subscriberTypeSummary$ = this.dashboardService.getBySubscriberType(this.subscriberTypeId).pipe(
-      finalize(() => this.blockSubscriberType.stop())
-    )
   }
 
   setProgressColor(percentage: number) {
@@ -89,154 +61,108 @@ export class DashboardComponent implements OnInit, AfterViewInit {
     } else { return 'bg-danger' }
   }
 
-  private loadSubscriberTypes() {
-    this.loadingSubscriberTypes = true
-    this.subscriberTypes$ = this.settingsService.fetch2('subscribertype').pipe(
-      map(rec => {
-        this.subscriberTypeId = rec[0].id
-        this.subscriberTypeOnChange()
-        return rec
-      }),
-      finalize(() => this.loadingSubscriberTypes = false)
+  yearOnChange() {
+    console.log(this.year)
+    this.blockYearlyStats.start('Loading...')
+    this.yearlyStats$ = this.dashboardService.getYearlyStatistics(this.year).pipe(
+      finalize(() => this.blockYearlyStats.stop())
     )
   }
 
-  private getSubscriberSummary() {
-    this.blockSubscriberSummary.start('Loading')
-    this.subscriberSummary$ = this.dashboardService.getSubscriberSummary().pipe(
-      finalize(() => this.blockSubscriberSummary.stop())
+  // private subscriberCommodityPie() {
+  //   this.blockCommodity.start('Loading...')
+  //   this.dashboardService.getCommoditySummary().subscribe(res => {
+  //     this.blockCommodity.stop()
+  //     if (res.success) {
+  //       const data = [res.data.cocoa, res.data.gold, res.data.palmOil];
+  //       const labels = ['Cocoa ' + res.data.cocoa, 'Gold ' + res.data.gold, 'Oil Palm ' + res.data.palmOil];
+  //       const total = res.data.cocoa + res.data.gold + res.data.palmOil;
+
+  //       // const data = [20, 30, 40, 50]
+  //       this.pie = new Chart(this.piecanvas.nativeElement, {
+  //         type: 'pie',
+  //         data: {
+  //           labels: labels,
+  //           datasets: [
+  //             {
+  //               data: data,
+  //               backgroundColor: ['#7a401b', '#ffc800', '#d81d1e', '#a3a0fb']
+  //             }
+  //           ]
+  //         },
+  //         options: {
+  //           legend: {
+  //             position: 'bottom',
+  //             labels: {
+  //               boxWidth: 10
+  //             }
+  //           },
+  //           responsive: true,
+  //           maintainAspectRatio: false
+  //         }
+  //       })
+  //     }
+  //   }, () => this.blockCommodity.stop())
+
+  // }
+
+  // private landAreaDoughnut() {
+  //   this.blockLand.start('Loading...')
+  //   this.dashboardService.getLandArea().subscribe(res => {
+  //     this.blockLand.stop()
+  //     if (res.success) {
+  //       const data = [res.data.cocoa, res.data.gold, res.data.palmOil];
+  //       const labels = ['Cocoa ' + res.data.cocoa + ' Hectares', 'Gold ' + res.data.gold + ' Hectares', 'Oil Palm ' + res.data.palmOil + ' Hectares'];
+  //       const total = res.data.cocoa + res.data.gold + res.data.palmOil;
+
+  //       // const data = [20, 30, 40, 50]
+  //       this.doughnut = new Chart(this.doughnutcanvas.nativeElement, {
+  //         type: 'doughnut',
+  //         data: {
+  //           labels: labels,
+  //           datasets: [
+  //             {
+  //               data: data,
+  //               backgroundColor: ['#7a401b', '#ffc800', '#d81d1e', '#a3a0fb']
+  //             }
+  //           ]
+  //         },
+  //         options: {
+  //           cutoutPercentage: 80,
+  //           legend: {
+  //             position: 'bottom',
+  //             labels: {
+  //               boxWidth: 10
+  //             }
+  //           },
+  //           responsive: true,
+  //           maintainAspectRatio: false,
+  //           elements: {
+  //             center: {
+  //               text: total,
+  //               color: '#797b85',
+  //               fontStyle: 'Helvetica',
+  //               sidePadding: -30
+  //             }
+  //           }
+  //         }
+  //       })
+  //     }
+  //   }, () => this.blockLand.stop())
+  // }
+
+  getGeneralStats() {
+    this.blockGeneralStats.start('Loading')
+    this.generalStats$ = this.dashboardService.getGeneralStatistics().pipe(
+      finalize(() => this.blockGeneralStats.stop())
     )
   }
 
-  private subscriberCommodityPie() {
-    this.blockCommodity.start('Loading...')
-    this.dashboardService.getCommoditySummary().subscribe(res => {
-      this.blockCommodity.stop()
-      if (res.success) {
-        const data = [res.data.cocoa, res.data.gold, res.data.palmOil];
-        const labels = ['Cocoa ' + res.data.cocoa, 'Gold ' + res.data.gold, 'Oil Palm ' + res.data.palmOil];
-        const total = res.data.cocoa + res.data.gold + res.data.palmOil;
-
-        // const data = [20, 30, 40, 50]
-        this.pie = new Chart(this.piecanvas.nativeElement, {
-          type: 'pie',
-          data: {
-            labels: labels,
-            datasets: [
-              {
-                data: data,
-                backgroundColor: ['#7a401b', '#ffc800', '#d81d1e', '#a3a0fb']
-              }
-            ]
-          },
-          options: {
-            legend: {
-              position: 'bottom',
-              labels: {
-                boxWidth: 10
-              }
-            },
-            responsive: true,
-            maintainAspectRatio: false
-          }
-        })
-      }
-    }, () => this.blockCommodity.stop())
-
-  }
-
-  private landAreaDoughnut() {
-    this.blockLand.start('Loading...')
-    this.dashboardService.getLandArea().subscribe(res => {
-      this.blockLand.stop()
-      if (res.success) {
-        const data = [res.data.cocoa, res.data.gold, res.data.palmOil];
-        const labels = ['Cocoa ' + res.data.cocoa + ' Hectares', 'Gold ' + res.data.gold + ' Hectares', 'Oil Palm ' + res.data.palmOil + ' Hectares'];
-        const total = res.data.cocoa + res.data.gold + res.data.palmOil;
-
-        // const data = [20, 30, 40, 50]
-        this.doughnut = new Chart(this.doughnutcanvas.nativeElement, {
-          type: 'doughnut',
-          data: {
-            labels: labels,
-            datasets: [
-              {
-                data: data,
-                backgroundColor: ['#7a401b', '#ffc800', '#d81d1e', '#a3a0fb']
-              }
-            ]
-          },
-          options: {
-            cutoutPercentage: 80,
-            legend: {
-              position: 'bottom',
-              labels: {
-                boxWidth: 10
-              }
-            },
-            responsive: true,
-            maintainAspectRatio: false,
-            elements: {
-              center: {
-                text: total,
-                color: '#797b85',
-                fontStyle: 'Helvetica',
-                sidePadding: -30
-              }
-            }
-          }
-        })
-      }
-    }, () => this.blockLand.stop())
-  }
-
-  getSustainabilityStatistics(params) {
-    this.blockCampaign.start('Loading')
-    this.susStatus$ = this.dashboardService.getSustainabilityStatistics(params).pipe(
-      finalize(() => this.blockCampaign.stop())
+  getYearlyStats() {
+    this.blockYearlyStats.start('Loading')
+    this.yearlyStats$ = this.dashboardService.getYearlyStatistics(this.year).pipe(
+      finalize(() => this.blockYearlyStats.stop())
     )
   }
 
-  private increaseMonth(date: Date, increament: number) {
-    date.setMonth(date.getMonth() + increament)
-    return date
-  }
-
-  private initDates() {
-    this.startDate = new Date().toISOString().substring(0, 10)
-    this.endDate = this.increaseMonth(new Date(), 1).toISOString().substring(0, 10)
-  }
-
-  private loadAreas() {
-    this.loadingAreas = true
-    this.areas$ = this.settingsService.fetch2('area').pipe(
-      finalize(() => this.loadingAreas = false)
-    )
-  }
-
-  private loadPillars() {
-    this.loadingPillars = true
-    this.pillars$ = this.settingsService.fetch2('pillar').pipe(
-      finalize(() => this.loadingPillars = false)
-    )
-  }
-
-  loadTopicsInPillar(pillarId: number) {
-    this.loadingTopics = true
-    this.topics$ = this.dashboardService.fetchTopicsByPillar(pillarId).pipe(
-      finalize(() => this.loadingTopics = false)
-    )
-  }
-  private loadPrograms() {
-    this.loadingPrograms = true
-    this.programs$ = this.settingsService.fetch2('program').pipe(
-      finalize(() => this.loadingPrograms = false)
-    )
-  }
-  private loadDistrict() {
-    this.loadingDistricts = true
-    this.districts$ = this.settingsService.fetch2('district').pipe(
-      finalize(() => this.loadingDistricts = false)
-    )
-  }
 }
--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -11,54 +11,19 @@ export class DashboardService {
 
   constructor(private http: HttpClient) { }
 
-  getSubscriberSummary() {
-    return this.http.get<ResponseObject<any>>(`${environment.baseUrl}/dashboard/subscribersummary`).pipe(
+  getGeneralStatistics() {
+    return this.http.get<ResponseObject<any>>(`${environment.baseUrl}/dashboard/generalstats`).pipe(
       map(res => {
         if (res.success) { return res.data }
       })
     )
   }
 
-  getBySubscriberType(id: number) {
-    return this.http.get<ResponseObject<any>>(`${environment.baseUrl}/dashboard/subscribersummarybytype?subscriberTypeId=${id}`).pipe(
+  getYearlyStatistics(year: number) {
+    return this.http.get<ResponseObject<any>>(`${environment.baseUrl}/dashboard/yearlystats?year=${year}`).pipe(
       map(res => {
         if (res.success) { return res.data }
       })
     )
-  }
-
-  getCommoditySummary() {
-    return this.http.get<ResponseObject<any>>(`${environment.baseUrl}/dashboard/subscribersummarybycommodity`)
-      // .pipe(
-      //   map(res => {
-      //     if (res.success) { return res.data }
-      //   })
-      // )
-  }
-
-  getLandArea() {
-    return this.http.get<ResponseObject<any>>(`${environment.baseUrl}/dashboard/landareabycommodity`)
-      // .pipe(
-      //   map(res => {
-      //     if (res.success) { return res.data }
-      //   })
-      // )
-  }
-
-  getSustainabilityStatistics(params) {
-    return this.http.post<ResponseObject<any>>(`${environment.baseUrl}/dashboard/getsustainabilitystatistics`, params).pipe(
-      map(res => {
-        if (res.success) { return res.data }
-      })
-    )
-  }
-
-  fetchTopicsByPillar(pillarId: number) {
-    return this.http.get<ResponseObject<Lookup[]>>(`${environment.baseUrl}/topic/gettopics?pillarId=${pillarId}`)
-      .pipe(
-        map(res => {
-          if (res.success) { return res.data }
-        })
-      )
   }
 }
